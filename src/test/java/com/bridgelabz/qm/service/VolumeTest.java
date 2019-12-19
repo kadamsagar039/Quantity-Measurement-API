cb8496aabe3cba9556c08686160fbd8bbd6c1ee8
Aniket Chile <aniket.chile@bridgelabz.com>
2019-12-19 11:25:05 +0530

    REFACTORED volume name to gallon in volume test

--- src/test/java/com/bridgelabz/qm/service/VolumeTest.java
+++ src/test/java/com/bridgelabz/qm/service/VolumeTest.java
@@ -8,19 +8,21 @@ public class VolumeTest {
 
     @Test
     void whenGiven0GallonAnd0Gallon_shouldReturnEquals() {
-        Volume volume = new Volume(VolumeUnit.GALLON, 0.0);
-        Assert.assertEquals(volume, new Volume(VolumeUnit.GALLON, 0.0));
+        Volume gallon = new Volume(VolumeUnit.GALLON, 0.0);
+        Assert.assertEquals(gallon, new Volume(VolumeUnit.GALLON, 0.0));
     }
 
     @Test
     void whenGiven1GallonAnd1Gallon_shouldReturnEquals() {
-        Volume volume = new Volume(VolumeUnit.GALLON, 1.0);
-        Assert.assertEquals(volume, new Volume(VolumeUnit.GALLON, 1.0));
+        Volume gallon = new Volume(VolumeUnit.GALLON, 1.0);
+        Assert.assertEquals(gallon, new Volume(VolumeUnit.GALLON, 1.0));
     }
 
     @Test
     void whenGiven1GallonAnd2Gallon_shouldReturnNotEqual() {
-        Volume volume = new Volume(VolumeUnit.GALLON, 1.0);
-        Assert.assertNotEquals(volume, new Volume(VolumeUnit.GALLON, 2.0));
+        Volume gallon = new Volume(VolumeUnit.GALLON, 1.0);
+        Assert.assertNotEquals(gallon, new Volume(VolumeUnit.GALLON, 2.0));
     }
+
+
 }
